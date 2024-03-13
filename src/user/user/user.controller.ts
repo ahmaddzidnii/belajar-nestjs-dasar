711c9dee3c4621c81cee3fc3f4c3f12dd3bfcda1
ahmaddzidnii <zdngaming312@gmail.com>
2024-03-13 14:42:35 +0700

    learn: Dependency Injection

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { UserService } from './user.service';
 
 @Controller('api/users')
 export class UserController {
+  constructor(private service: UserService) {}
   @Get('/hello')
-  async sayHello(@Query('name') name: string): Promise<object> {
-    return {
-      message: `Hello ${name}`,
-    };
+  async sayHello(@Query('name') name: string): Promise<string> {
+    return this.service.sayHello(name);
   }
 
   @Get('/set-cookie')
